693d8c98399d9cf610ea3d9274a0bcabae8b633e
haseeb2306e@aptechgdn.net <haseeb2306e@aptechgdn.net>
2023-12-16 12:01:17 +0500

    Telemart

---
+++ TeleMart/JS/cart.js
@@ -0,0 +1,6 @@
+function addprice(){
+    document.getElementById("cartpro1").style.display="block";
+    var price1=60000
+    document.getElementById("price").innerText.style.display="none"
+}
+document.getElementById("price") .styele.display="none"
---
+++ TeleMart/JS/index.js
@@ -0,0 +1,357 @@
+// $(document).ready(function ($) {
+// 	$(".owl-carousel").owlCarousel({
+// 		loop: false,
+// 		margin: 10,
+// 		dots: false,
+// 		nav: true,
+// 		items: 5 
+// 	});
+// 	var owl = $(".owl-carousel");
+// 	owl.owlCarousel();
+// 	$(".next-btn").click(function () {
+// 		owl.trigger("next.owl.carousel");
+// 	});
+// 	$(".prev-btn").click(function () {
+// 		owl.trigger("prev.owl.carousel");
+// 	});
+// 	$(".prev-btn").addClass("disabled");
+// 	$(owl).on("translated.owl.carousel", function (event) {
+// 		if ($(".owl-prev").hasClass("disabled")) {
+// 			$(".prev-btn").addClass("disabled");
+// 		} else {
+// 			$(".prev-btn").removeClass("disabled");
+// 		}
+// 		if ($(".owl-next").hasClass("disabled")) {
+// 			$(".next-btn").addClass("disabled");
+// 		} else {
+// 			$(".next-btn").removeClass("disabled");
+// 		}
+// 	});
+   
+// });
+ 
+  
+      function hover(){
+        var mobile = document.getElementById("mobile")
+        var sidemobile = document.getElementById("sidenavbar").style.display="block"
+           
+      }
+      function hoverout(){
+        var mobile = document.getElementById("mobile")
+        var sidemobile = document.getElementById("sidenavbar").style.display="none"
+      }
+      function hover2(){
+        var men = document.getElementById("menfashion")
+        var sidemobile2 = document.getElementById("sidenavbar2").style.display="block"
+           
+      }
+      function hoverout2(){
+        var men = document.getElementById("menfashion")
+        var sidemobile2 = document.getElementById("sidenavbar2").style.display="none"
+      }
+      function hover3(){
+        var women = document.getElementById("womenfashion")
+        var sidemobile3 = document.getElementById("sidenavbar3").style.display="block"
+           
+      }
+      function hoverout3(){
+        var women = document.getElementById("womenfashion")
+        var sidemobile3 = document.getElementById("sidenavbar3").style.display="none"
+      }
+      function hover4(){
+        var appliances = document.getElementById("appliances")
+        var sidemobile4 = document.getElementById("sidenavbar4").style.display="block"
+           
+      }
+      function hoverout4(){
+        var appliances = document.getElementById("appliances")
+        var sidemobile4 = document.getElementById("sidenavbar4").style.display="none"
+      }
+      function hover5(){
+        var tv = document.getElementById("tv")
+        var sidemobile5 = document.getElementById("sidenavbar5").style.display="block"
+           
+      }
+      function hoverout5(){
+        var tv = document.getElementById("tv")
+        var sidemobile5 = document.getElementById("sidenavbar5").style.display="none"
+      }
+      function hover6(){
+        var kids = document.getElementById("kids")
+        var sidemobile6 = document.getElementById("sidenavbar6").style.display="block"
+           
+      }
+      function hoverout6(){
+        var kids = document.getElementById("kids")
+        var sidemobile6 = document.getElementById("sidenavbar6").style.display="none"
+      }
+      function hover7(){
+        var home = document.getElementById("home")
+        var sidemobile7 = document.getElementById("sidenavbar7").style.display="block"
+           
+      }
+      function hoverout7(){
+        var home = document.getElementById("home")
+        var sidemobile7 = document.getElementById("sidenavbar7").style.display="none"
+      }
+      function hover8(){
+        var computer = document.getElementById("computer")
+        var sidemobile8 = document.getElementById("sidenavbar8").style.display="block"
+           
+      }
+      function hoverout8(){
+        var computer = document.getElementById("computer")
+        var sidemobile8 = document.getElementById("sidenavbar8").style.display="none"
+      }
+      function hover9(){
+        document.getElementById("automoto")
+          document.getElementById("sidenavbar9").style.display="block"
+           
+      }
+      function hoverout9(){
+         document.getElementById("automoto")
+         document.getElementById("sidenavbar8").style.display="none"
+      }
+      function hover10(){
+         document.getElementById("care")
+        document.getElementById("sidenavbar10").style.display="block"
+           
+      }
+      function hoverout10(){
+          document.getElementById("care")
+          document.getElementById("sidenavbar10").style.display="none"
+      }
+      function hover11(){
+          document.getElementById("camera")
+         document.getElementById("sidenavbar11").style.display="block"
+           
+      }
+      function hoverout11(){
+         document.getElementById("camera")
+        document.getElementById("sidenavbar11").style.display="none"
+      }
+      function hover12(){
+         document.getElementById("theatre")
+        document.getElementById("sidenavbar12").style.display="block"
+           
+      }
+      function hoverout12(){
+       document.getElementById("theatre")
+      document.getElementById("sidenavbar12").style.display="none"
+      }
+      function hover13(){
+        document.getElementById("more")
+         document.getElementById("sidenavbar13").style.display="block"
+           
+      }
+      function hoverout13(){
+          document.getElementById("more")
+          document.getElementById("sidenavbar13").style.display="none"
+      }
+      function side1(){
+        
+        var sidemobile = document.getElementById("sidenavbar").style.display="block"
+           
+      }
+      function side1out(){
+        
+       document.getElementById("sidenavbar").style.display="none"
+      }
+      function side2(){
+        
+      document.getElementById("sidenavbar2").style.display="block"
+           
+      }
+      function side2out(){
+        
+         document.getElementById("sidenavbar2").style.display="none"
+      }
+      function side3(){
+        
+        document.getElementById("sidenavbar3").style.display="block"
+             
+        }
+        function side3out(){
+          
+           document.getElementById("sidenavbar3").style.display="none"
+        }
+        function side4(){
+        
+        document.getElementById("sidenavbar4").style.display="block"
+             
+        }
+        function side4out(){
+          
+           document.getElementById("sidenavbar4").style.display="none"
+        }
+        function side5(){
+        
+        document.getElementById("sidenavbar5").style.display="block"
+             
+        }
+        function side5out(){
+          
+           document.getElementById("sidenavbar5").style.display="none"
+        }
+        function side6(){
+        
+        document.getElementById("sidenavbar6").style.display="block"
+             
+        }
+        function side6out(){
+          
+           document.getElementById("sidenavbar6").style.display="none"
+        }
+        function side7(){
+        
+        document.getElementById("sidenavbar7").style.display="block"
+             
+        }
+        function side7out(){
+          
+           document.getElementById("sidenavbar7").style.display="none"
+        }
+        function side8(){
+        
+        document.getElementById("sidenavbar8").style.display="block"
+             
+        }
+        function side8out(){
+          
+           document.getElementById("sidenavbar8").style.display="none"
+        }
+        function side9(){
+        
+            document.getElementById("sidenavbar9").style.display="block"
+                 
+            }
+            function side9out(){
+              
+               document.getElementById("sidenavbar9").style.display="none"
+            }
+            function side10(){
+        
+                document.getElementById("sidenavbar10").style.display="block"
+                     
+                }
+                function side10out(){
+                  
+                   document.getElementById("sidenavbar10").style.display="none"
+                }         
+                function side11(){
+        
+                    document.getElementById("sidenavbar11").style.display="block"
+                         
+                    }
+                    function side11out(){
+                      
+                       document.getElementById("sidenavbar11").style.display="none"
+                    }   
+                    function side12(){
+        
+                        document.getElementById("sidenavbar12").style.display="block"
+                             
+                        }
+                        function side12out(){
+                          
+                           document.getElementById("sidenavbar12").style.display="none"
+                        }   
+                        function side13(){
+        
+                            document.getElementById("sidenavbar13").style.display="block"
+                                 
+                            }
+                            function side13out(){
+                              
+                               document.getElementById("sidenavbar13").style.display="none"
+                            }   
+                            function barhover(){
+        
+                              document.getElementById("produtsidenav").style.display="block"
+                                   
+                              }
+                              function barhover2(){
+                                
+                                 document.getElementById("produtsidenav").style.display="none"
+                              }  
+                              function sidenavhov(){
+                                document.getElementById("produtsidenav").style.display="block"
+                              } 
+                              function sidenavhov2(){
+                                document.getElementById("produtsidenav").style.display="none"
+                              }   
+     function proimg(){
+      document.getElementById("changeimg1").src="./Images/flash10.gif"
+     }  
+     function proimg2() {
+      document.getElementById("changeimg1").src="./Images/flash3.jpg"
+
+
+     }   
+     function descplus(){
+      document.getElementById("minusicon").style.display="block"
+      document.getElementById("plusicon").style.display="none"
+      document.getElementById("dropdown").style.display="block"
+     }
+     function descminus(){
+      document.getElementById("minusicon").style.display="none"
+      document.getElementById("plusicon").style.display="block"
+      document.getElementById("dropdown").style.display="none"
+
+
+
+
+     }
+     
+     
+ 
+ 
+let data = 0;
+ 
+ 
+document.getElementById("counting").value = data;
+ 
+ 
+function increment() {
+    data = data + 1;
+    document.getElementById("counting").value = data;
+}
+ 
+function decrement() {
+    data = data - 1;
+    document.getElementById("counting").value = data;}
+
+
+    // counter in cart
+    document.getElementById("countingcart").value = data;
+ 
+ 
+function increment() {
+    data = data + 1;
+    document.getElementById("countingcart").value = data;
+}
+ 
+function decrement() {
+    data = data - 1;
+    document.getElementById("countingcart").value = data;
+}
+const myID = document.getElementById("myID");
+
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 300) {
+        myID.className = "bottomMenu show"
+    } else {
+        myID.className = "bottomMenu hide"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
+                                        //  Product Menuebar
+ 
+
+ function addprice(){
+  var a = document.getElementById("proprice").innerHTML
+  var b =document.getElementById("price").innerHTML
+  b.innerHTML="a"
+ }
